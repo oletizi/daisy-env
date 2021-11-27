b01c2f2f60bd483788f099d53460ab1a77632465
orion <oletizi@mac.com>
2021-11-27 11:16:10 -0800

    getting closer to a basic setup.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
-#include <cstdio>
 #include "Utility/metro.h"
 #include <plog/Log.h> // Step1: include the headers
 #include <plog/Appenders/ColorConsoleAppender.h>
+#include <gtest/gtest.h>
 
 int main() {
     static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
     plog::init(plog::debug, &consoleAppender);
+
     daisysp::Metro metro;
     metro.Init(1000, 48000);
 
     int hits = 0;
-
     for (int i=0; i<48000; i++) {
         hits += metro.Process();
     }
--- test.cpp
+++ test.cpp
@@ -1,4 +1,9 @@
-//
-// Created by Orion Letizi on 11/27/21.
-//
+#include <plog/Log.h> // Step1: include the headers
+#include <plog/Appenders/ColorConsoleAppender.h>
 
+int main() {
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::debug, &consoleAppender);
+
+    PLOGI.printf("I'm testing!!!");
+}
