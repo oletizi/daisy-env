c792d379020bb3166800298a7a2fffe3f5e5227c
orion <oletizi@mac.com>
2021-11-26 18:05:04 -0800

    - Added conan support - Added Plog

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <cstdio>
 #include "Utility/metro.h"
+#include <plog/Log.h> // Step1: include the headers
+#include <plog/Appenders/ColorConsoleAppender.h>
 
 int main() {
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::debug, &consoleAppender);
     daisysp::Metro metro;
     metro.Init(1000, 48000);
-    std::cout << "Hello, World!" << std::endl;
+    uint8_t processed = metro.Process();
+
+    PLOGI.printf("Processed: %d", processed);
+    PLOGI.printf("Hello, log");
+
     return 0;
 }
